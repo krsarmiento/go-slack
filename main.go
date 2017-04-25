73a78e4a868435cf593dadfb12cb622cca3214fb
Kevin Sarmiento <ksarmiento@clickdelivery.com>
2017-04-25 18:00:01 -0500

    Adding router package to handle router mapping between socket emits and its corresponding methods.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
     "fmt"
-    "github.com/gorilla/websocket"
     "net/http"
     "github.com/mitchellh/mapstructure"
     "time"
@@ -18,79 +17,10 @@ type Channel struct {
     Name string `json:"name"`
 }
 
-var upgrader = websocket.Upgrader {
-    ReadBufferSize: 1024,
-    WriteBufferSize: 1024,
-    CheckOrigin: func(r *http.Request) bool { return true },
-}
-
 func main() {
-    http.HandleFunc("/", handler)
+    router := NewRouter()
+    router.Handle("channel add", addChannel)
+
+    http.Handle("/", router)
     http.ListenAndServe(":4000", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    socket, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    for {
-        // msgType, msg, err := socket.ReadMessage()
-        // if err != nil {
-        //     fmt.Println(err)
-        //     return
-        // }
-
-        var inMessage Message
-        var outMessage Message
-        if err := socket.ReadJSON(&inMessage); err != nil {
-            fmt.Println(err)
-            break
-        }
-        fmt.Printf("%#v\n", inMessage)
-
-        switch inMessage.Name {
-        case "channel add":
-            err := addChannel(inMessage.Data)
-            if err != nil {
-                outMessage = Message{"error", err}
-                if err := socket.WriteJSON(outMessage); err != nil {
-                    fmt.Println(err)
-                    break
-                }
-            }
-        case "channel subscribe":
-            go subscribeChannel(socket)
-        }
-        // fmt.Println(string(msg))
-        // if err = socket.WriteMessage(msgType, msg); err != nil {
-        //     fmt.Println(err)
-        //     return
-        // }
-    }
-}
-
-func addChannel(data interface{}) (error) {
-    var channel Channel
-
-    err := mapstructure.Decode(data, &channel)
-    if err != nil {
-        return err
-    }
-
-    channel.Id = "1"
-    fmt.Println("added channel")
-    return nil
-}
-
-func subscribeChannel(socket *websocket.Conn) {
-    // TODO: rethinkDB Query / changefeed
-    for {
-        time.Sleep(time.Second * 1)
-        message := Message{"channel add", 
-            Channel{"1", "Software Support"}}
-        socket.WriteJSON(message)
-        fmt.Println("sent new channel")
-    }
 }
